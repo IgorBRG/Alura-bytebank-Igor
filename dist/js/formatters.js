4f6cd25eb5f0f100191b69c4178e9d6738958863
Igor Oliveira <Igor Pc Pessoal>
2025-04-23 20:04:01 -0300

    estudos dia 23/04

---
+++ dist/js/formatters.js
@@ -0,0 +1,23 @@
+function formatarMoeda(valor) {
+    return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+}
+function formatarData(data, formato = FormatoData.PADRAO) {
+    if (formato == FormatoData.DIA_SEMANA_DIA_MES_ANO) {
+        return data.toLocaleDateString("pt-BR", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            weekday: "long",
+        });
+    }
+    else if (formato == FormatoData.DIA_MES) {
+        return data.toLocaleDateString("pt-BR", {
+            month: "2-digit",
+            day: "2-digit",
+        });
+    }
+    return data.toLocaleDateString("pt-BR");
+}
---
+++ src/formatters.ts
@@ -0,0 +1,25 @@
+function formatarMoeda(valor: number): string {
+  return valor.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
+
+function formatarData(data: Date, formato: FormatoData=FormatoData.PADRAO): string {
+    if( formato==FormatoData.DIA_SEMANA_DIA_MES_ANO){
+        return data.toLocaleDateString("pt-BR", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            weekday: "long",
+        });
+    }
+    else if (formato==FormatoData.DIA_MES){
+        return data.toLocaleDateString("pt-BR", {
+            month: "2-digit",
+            day: "2-digit",
+        });
+    }
+    return data.toLocaleDateString("pt-BR");
+
+}
